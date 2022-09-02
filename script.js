26aa8dfa0e7074e6fd4c63eab0b1a4acc6c86ce9
Dinara Idrissova <Dinara.Idrissova@list.ru>
2022-09-01 22:29:45 -0400

    practice for loop, conditional && logical statements, finish the project

--- script.js
+++ script.js
@@ -3,29 +3,49 @@ const newCard = document.querySelector("#new-card");
 const messageEl = document.getElementById("message-el");
 const sumEl = document.getElementById("sum-el");
 const cardsEl = document.getElementById("cards-el");
+const player = document.getElementById("player");
 
 startBtn.addEventListener("click", startGame);
 newCard.addEventListener("click", newCardFunc);
 
-function getRandomCard() {
-  return Math.floor(Math.random() * 13) + 1;
-}
+let playerInfo = {
+  name: "John",
+  chips: 120,
+};
 
-let firstCard = getRandomCard();
-let secondCard = getRandomCard();
-let cards = [firstCard, secondCard]; //array of cards
-let sum = firstCard + secondCard;
+player.textContent = playerInfo.name + ": $" + playerInfo.chips;
+
+let cards = []; //array of cards
+let sum = 0;
 let hasBlackJack = false;
-let isAlive = true;
+let isAlive = false;
 
 let message = "";
 
 function startGame() {
+  isAlive = true;
+  let firstCard = getRandomCard();
+  let secondCard = getRandomCard();
+  cards = [firstCard, secondCard];
+  sum = firstCard + secondCard;
   renderGame();
+}
+
+function getRandomCard() {
+  let randomNumber = Math.floor(Math.random() * 13) + 1;
+  if (randomNumber === 1) {
+    return 11;
+  } else if (randomNumber > 10) {
+    return 10;
+  } else {
+    return randomNumber;
+  }
+  return randomNumber;
 }
 
 function renderGame() {
   // console.log("game started");
+
   cardsEl.textContent = "Cards: ";
 
   for (let i = 0; i < cards.length; i++) {
@@ -46,11 +66,15 @@ function renderGame() {
 }
 
 function newCardFunc() {
-  let card = getRandomCard();
-  sum += card;
-  cards.push(card);
-  renderGame();
+  if (isAlive && hasBlackJack === false) {
+    let newCard = getRandomCard();
+    sum += newCard;
+    cards.push(newCard);
+    renderGame();
+  }
 }
+
+console.log(cards);
 
 // const sentence = ["Hello", "my", "name", "is", "Dina"];
 // let greetingEl = document.getElementById("greeting-el");
@@ -84,3 +108,83 @@ function newCardFunc() {
 // }
 
 // console.log(rollDice());
+
+// Practice - Conditional
+// let age = 67;
+// if (age < 6) {
+//   console.log("free");
+// } else if (age < 18) {
+//   console.log("child discount");
+// } else if (age < 27) {
+//   console.log("student discount");
+// } else if (age < 67) {
+//   console.log("full price");
+// } else {
+//   console.log("senior citizen discount");
+// }
+
+//Practice - Loops& Arrays
+// const largeCountries = ["China", "India", "USA", "Indonesia", "Pakistan"];
+
+// console.log(`The ${largeCountries.length} largest countries in the world: `);
+// for (let i = 0; i < largeCountries.length; i++) {
+//   console.log(`- ${largeCountries[i]}`);
+// }
+
+//Practice = push(), pop(), unshift(), shift()
+
+// const largeCountries = ["Tuvalu", "India", "USA", "Indonesia", "Monaco"];
+// largeCountries.pop();
+// largeCountries.shift();
+// largeCountries.unshift("China");
+// largeCountries.push("Pakistan");
+
+// console.log(largeCountries);
+
+//Practica - Logical and Conditional operators
+// let dayOfMonth = 13;
+// let weekday = "Friday";
+// if (dayOfMonth === 13 && weekday === "Friday") {
+//   console.log("😱");
+// }
+
+//Practice - 'Rock paper & scissors'
+
+// let hands = ["rock", "paper", "scissor"];
+
+// function gameStart() {
+//   console.log(hands[Math.floor(Math.random() * 3)]);
+// }
+// gameStart();
+//second version
+// function getHand() {
+//   let randomIndex = Math.floor(Math.random() * 3);
+//   return hands[randomIndex];
+// }
+
+// console.log(getHand());
+
+// Practice - for loop, a conditional statement, textContent property
+
+let fruit = ["🍏", "🍎", "🍊", "🍏", "🍊", "🍊"];
+let appleShelf = document.querySelector(".apple-shelf");
+let orangeShelf = document.querySelector(".orange-shelf");
+
+let apple = [];
+let orange = [];
+function sortFruit() {
+  for (let i = 0; i < fruit.length; i++) {
+    if (fruit[i] === "🍏" || fruit[i] === "🍎") {
+      apple.push(fruit[i]);
+      appleShelf.textContent = apple;
+    } else {
+      orange.push(fruit[i]);
+      orangeShelf.textContent = orange;
+    }
+  }
+}
+
+// console.log(apple);
+// console.log(orange);
+
+sortFruit();
